5a5302f2de69a5211ac72aed7ec4601ce91291ff
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2022-12-26 21:59:41 +0100

    declare variables

---
+++ learn.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void declareVariables() {
+    cout << "Declare Variables" << endl;
+
+    int myNum = 15;
+    cout << myNum << endl;
+
+    string greeting = "Hello";
+    cout << greeting << endl;
+}
+
+
+int main() {
+    cout << "Hello, World!" << std::endl;
+    declareVariables();
+    return 0;
+}
